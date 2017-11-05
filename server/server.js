085f78fd1d0aa3c1ec1495e4e780eaf900f480dc
Tyler Reitz <tdreitz@gmail.com>
2017-11-04 20:03:20 -0700

    sessions complete

--- server/server.js
+++ server/server.js
@@ -1,7 +1,24 @@
 const express = require('express')
+const uuid = require('uuid/v4')
+const session = require('express-session')
+const FileStore = require('session-file-store')(session)
 
 const app = express()
 
-app.get('/', (req, res) => res.send('Hey-lo'))
+app.use(session({
+  genid: (req) => {
+    console.log(`from inside session middleware: ${req.sessionID}`)
+    return uuid()
+  },
+  store: new FileStore(),
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}))
+
+app.get('/', (req, res) => {
+  console.log(`from inside / callback: ${req.sessionID}`)
+  res.send(`You hit the homepage son!\n`)
+})
 
 app.listen(3000, () => console.log('Listening on 3000'))
